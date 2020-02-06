88435914161f9674e93741d998c99f4ea9ce73e5
iliasbolt <ana.@ana.com>
2020-02-06 09:24:24 +0000

    frmaraarticle

--- Syndic/FrmAMArticle.cs
+++ Syndic/FrmAMArticle.cs
@@ -16,5 +16,10 @@ namespace Syndic
         {
             InitializeComponent();
         }
+
+        private void btn_valider_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ok");
+        }
     }
 }
